89da3794061c512ee8a5a42da83460515e1a3ab8
Tomas Trescak <tomi.trescak@gmail.com>
2025-02-12 08:51:38 +1100

    Initial Setup

--- tests/storybook/stories/Button.stories.ts
+++ tests/storybook/stories/Button.stories.ts
@@ -14,10 +14,10 @@ const meta = {
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    appName: { control: "text" },
+    title: { control: "text" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { appName: "World" },
+  args: { title: "World" },
 } satisfies Meta<typeof Button>;
 
 export default meta;
